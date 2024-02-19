8f9238757835bff063c4f9a6dccaee828334cbc5
Andrew Bedoy <116923223+AndrewBedoy@users.noreply.github.com>
2024-02-18 23:10:39 -0600

    minus

--- cesar.js
+++ cesar.js
@@ -1,6 +1,6 @@
 
 function encrypt() {
-    var text = document.getElementById("text");//.value.toUpperCase();
+    var text = document.getElementById("text").value.toUpperCase();
     var shift = parseInt(document.getElementById("shift").value);
     var result = "";
     //1
@@ -33,11 +33,11 @@ function encrypt() {
             result += text.charAt(i); //6   
         }
     }
-    document.getElementById("result").value = result;
+    document.getElementById("result").value.toLowerCase() = result;
 }
 
 function decrypt() {
-    var text = document.getElementById("text");//.value.toUpperCase();
+    var text = document.getElementById("text").value.toUpperCase();
     var shift = parseInt(document.getElementById("shift").value);
     var result = "";
     
@@ -70,6 +70,6 @@ function decrypt() {
             result += text.charAt(i); //12    
         }
     }
-    document.getElementById("result").value = result;
+    document.getElementById("result").value.toLowerCase()= result;
 }
 
